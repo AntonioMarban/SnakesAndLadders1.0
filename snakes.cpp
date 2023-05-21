7c3f40a204e085d97a918b6049b70acc857f029f
CarolRendonG <114258527+CarolRendonG@users.noreply.github.com>
2023-05-21 10:03:00 -0600

    Update snakes.cpp
    nueva clase my game para inicar el juego

--- snakes.cpp
+++ snakes.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include "classes/Turn.h"
 using namespace std;
 
 class MyGame {
+    private:
+
     public:
-        void start() {
-            
-        }
+        MyGame();
+        ~MyGame();
+        void start();
 };
 
+MyGame::MyGame(){}
+MyGame::~MyGame(){}
+
+void MyGame::start(){
+    char s1;
+    cout << "Press C to continue next turn, or E to end the game:"<< endl;
+    cin >> s1;
+    while(s1!='C' && s1!='E' ){
+    cout<< "Invalid option, please press C to continue next turn or E to end the game."<<endl;
+    cin >> s1;
+    }
+    if (s1=='C'){
+        cout<< "Continuar"<<endl;
+        Turn a;
+    }
+    if (s1=='E'){
+        cout<< "Exit"<<endl;
+    }
+
+
+}
+
 int main() {
+    MyGame g; 
+    g.start();
 
 }
